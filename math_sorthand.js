0f9f7970076c1109324493f76d766504916ad9fd
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-03-27 15:11:35 +0600

    orangePrice shorthand variable added

--- math_sorthand.js
+++ math_sorthand.js
@@ -2,3 +2,12 @@ var orangePrice = 450;
 var tomatoPrice = 210;
 var totalPrice = orangePrice + tomatoPrice;
 console.log(totalPrice)
+
+
+// orangePrice = orangePrice + 10;
+// console.log('line 8', orangePrice)
+
+// Shorthand variable
+orangePrice += 10;
+console.log('line 12', orangePrice)
+
