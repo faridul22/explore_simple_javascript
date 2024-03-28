2d43d2bad181eb68d8f2a56e96dc881f2d525b1c
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-03-28 22:40:18 +0600

    mathmetical operation

---
+++ math_shorthand.js
@@ -0,0 +1,29 @@
+var orangePrice = 450;
+var tomatoPrice = 210;
+var totalPrice = orangePrice + tomatoPrice;
+console.log(totalPrice)
+
+// addition by main variable
+// orangePrice = orangePrice + 10;
+// console.log('line 8', orangePrice)
+
+
+// addition by Shorthand variable
+orangePrice += 10;
+console.log('line 12', orangePrice)
+
+// multiply
+let multiply = 9;
+multiply = multiply * 9;
+console.log(multiply)
+
+multiply *= 9;
+console.log(multiply)
+
+// Division
+let division = 10;
+division = division / 2;
+console.log(division)
+
+division /= 2;
+console.log(division)
