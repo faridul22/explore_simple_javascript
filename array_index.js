f8f7db5d383a23e085c35359085833627d30a09f
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-04-18 10:19:16 +0600

    array index explore done

---
+++ array_index.js
@@ -0,0 +1,13 @@
+const numbers = [12, 14, 16, 18, 20];
+
+// get third index of element
+const thirdIndex = numbers[3];
+console.log(thirdIndex);
+
+// set second index element
+numbers[2] = 22;
+console.log(numbers)
+
+// get an element index
+const elementIndex = numbers.indexOf(18);
+console.log(elementIndex);
